43315d9f002e2868e12eb04baa421896993c1dd1
ReillySullivan1 <64493883+ReillySullivan1@users.noreply.github.com>
2024-08-20 11:57:58 -0600

    update

--- script.js
+++ script.js
@@ -17,6 +17,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const resumeButton = document.getElementById('resume-button');
     resumeButton.addEventListener('click', function() {
-        window.location.href = 'Images/Resume.pdf'; // Replace with the actual path to your resume file
+        window.location.href = '/Images/Resume.pdf'; // Replace with the actual path to your resume file
     });
 });
